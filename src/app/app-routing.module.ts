925cea05891deb4c9ed3e9765738e4373256ac67
alex.key <alex.key@kawunu.com>
2021-08-15 21:54:47 +0200

    add product details and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MainpageComponent} from './mainpage/mainpage.component'
+import {MainpageComponent} from './mainpage/mainpage.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
 
 const routes: Routes = [
-  {path: '', component: MainpageComponent}
+  {path: '', component: MainpageComponent},
+  {path: 'product-details/:id', component: ProductDetailsComponent}
 ];
 
 @NgModule({
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {DataService} from "../service/data.service";
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private sub: any;
+  id: number = 0;
+  item: any = null;
+
+  constructor(private route: ActivatedRoute, private dataService: DataService) {
+  }
 
   ngOnInit(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params['id'];
+    })
+
+    this.dataService.getProducts().subscribe(products => {
+      this.item = products.filter( (product: any) => {
+        return product.id === this.id
+      })
+    })
+
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
